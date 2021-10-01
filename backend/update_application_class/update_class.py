3de3d45998af7bd3492219957f2cc086ff90054e
prithvish-doshi-17 <doshi.prithvish@gmail.com>
2021-09-30 20:50:41 -0400

    Updated csv location and minor change in logic

--- backend/update_application_class/update_class.py
+++ backend/update_application_class/update_class.py
@@ -1,11 +1,12 @@
+import os
 import pandas as pd
 import datetime
-
-df = pd.read_csv(r"<location of csv>")
-for row in df.iterrows():
+pd.set_option('display.max_columns', None)
+df = pd.read_csv(os.path.realpath(r"../data/applications.csv"), header=0)
+for index, row in df.iterrows():
     d1 = datetime.datetime.strptime(row['date'], "%Y-%m-%d")
-    d2 = datetime.date.today()
-    if (abs((d2 - d1).days) == 0) and (row['class'] == 3):
-        row['class'] = 2
-df.to_csv(r"<location of csv>")
+    d2 = datetime.datetime.today()
+    if (d2 - d1).days >= 0 and (row['class'] == 3):
+        df.at[index, 'class'] = 2
+df.to_csv(os.path.realpath(r"../data/applications.csv"), index=False)
 
